7c5bb9b8839d8424a24914abe46a80bce6319a63
Eduardo Gomes <gomeseduardogg@gmail.com>
2026-03-12 21:35:09 -0300

    resolvido erro de importação de módulos e implementa validação de chaves e assinatura digital

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 from crypto.key_generator import gerar_chaves
 from services.sender import enviar_contrato
 from services.receiver import receber_contrato
+from cryptography.hazmat.primitives import serialization
 
 # gerar chaves
 gerar_chaves("empresaA")
 gerar_chaves("empresaB")
 
+def carregar_chave_privada(caminho):
+    with open(caminho, "rb") as key_file:
+        return serialization.load_pem_private_key(
+            key_file.read(),
+            password=None
+        )
+
+def carregar_chave_publica(caminho):
+    with open(caminho, "rb") as key_file:
+        return serialization.load_pem_public_key(
+            key_file.read()
+        )
+
+# Carregando chaves em memória
+private_key_A = carregar_chave_privada("keys/empresaA_private.pem")
+public_key_A = carregar_chave_publica("keys/empresaA_public.pem")
+private_key_B = carregar_chave_privada("keys/empresaB_private.pem")
+public_key_B = carregar_chave_publica("keys/empresaB_public.pem")
+
 with open("contratos/contrato.txt", "rb") as f:
     contrato = f.read()
 
-pacote = enviar_contrato(contrato)
+# Fluxo da criptografia simétrica AES para o documento
+# e proteção da chave AES com RSA e OAEP acontecem no envio
+pacote = enviar_contrato(contrato, public_key_B, private_key_A)
 
-contrato_recuperado = receber_contrato(pacote)
+# Garantia de autenticidade através da assinatura digital RSA com PSS e decifração
+contrato_recuperado = receber_contrato(pacote, private_key_B, public_key_A)
 
 with open("contratos/contrato_recebido.txt", "wb") as f:
     f.write(contrato_recuperado)
 
-print("Contrato recebido com sucesso")
+if contrato == contrato_recuperado:
+    print("Sucesso na integridade: O contrato recuperado é idêntico ao original.")
+else:
+    print("Falha na integridade: Os contratos são diferentes.")
+
+print("Contrato processado com sucesso.")
--- services/receiver.py
+++ services/receiver.py
@@ -1,11 +1,18 @@
 from crypto.asymmetric_crypto import decrypt_key
 from crypto.symmetric_crypto import decrypt_aes
 from crypto.digital_signature import verify_signature
+from cryptography.exceptions import InvalidSignature
+import sys
 
 
 def receber_contrato(pacote, private_key_B, public_key_A):
 
-    verify_signature(public_key_A, pacote["signature"], pacote["doc"])
+    try:
+        # Garantia de autenticidade através da assinatura digital RSA com PSS
+        verify_signature(public_key_A, pacote["signature"], pacote["doc"])
+    except InvalidSignature:
+        print("Alerta de possível fraude: a assinatura do documento é inválida!")
+        sys.exit(1)
 
     aes_key = decrypt_key(private_key_B, pacote["key"])
 
